dca35f520dbd86d291d19161745495dfeeb2d2e7
deepanshusri <103935065+deepanshusri@users.noreply.github.com>
2024-09-20 00:26:06 +0530

    Update TestNGTodo1.java

--- src/test/java/com/lambdatest/TestNGTodo1.java
+++ src/test/java/com/lambdatest/TestNGTodo1.java
@@ -1,13 +1,24 @@
 package com.lambdatest;
 
-import java.lang.reflect.Method;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.openqa.selenium.By;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.Assert;
+import org.openqa.selenium.remote.SessionId;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,95 +27,261 @@ import org.testng.annotations.Test;
 public class TestNGTodo1 {
 
     private RemoteWebDriver driver;
-    private String Status = "failed";
+    private String testStatus;
 
     @BeforeMethod
     public void setup(Method m, ITestContext ctx) throws MalformedURLException {
-        String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
-        String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+        String username = "deepanshulambdatest";
+        String accessKey = "SMKwpdntRhBpUbdLrUqk0tU0TjAZquV9kz0YxnOWUo56EaqtQw";
         String hub = "@hub.lambdatest.com/wd/hub";
 
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platform", "MacOS Catalina");
-        caps.setCapability("browserName", "Safari");
-        caps.setCapability("version", "latest");
-        caps.setCapability("build", "TestNG With Java");
-        caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
-        caps.setCapability("plugin", "git-testng");
-        caps.setCapability("tunnel", true);
-
-
-        String[] Tags = new String[] { "Feature", "Falcon", "Severe" };
-        caps.setCapability("tags", Tags);
-
-        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
-
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        HashMap<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("platform", "windows 11");
+        ltOptions.put("browserName", "chrome");
+        ltOptions.put("version", "125");
+        ltOptions.put("build", "Qmetry test case build");
+        ltOptions.put("console", true);
+        ltOptions.put("visual", true);
+        ltOptions.put("network", true);
+        ltOptions.put("name", "SCRUM-TC-");
+        ltOptions.put("w3c", true);
+        capabilities.setCapability("lt:options", ltOptions);
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + hub), capabilities);
     }
 
     @Test
-    public void basicTest() throws InterruptedException {
-        String spanText;
-        System.out.println("Loading Url");
+    public void basicTest() {
+        driver.get("https://lambdatest.com/");
+        testStatus = "passed";
+    }
 
-        driver.get("https://lambdatest.github.io/sample-todo-app/");
+    private String getGETApiResponse(String apiUrl, String requestPropertyKey, String requestPropertyValue) throws IOException {
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("accept", "application/json");
+        connection.setRequestProperty(requestPropertyKey, requestPropertyValue);
 
-        System.out.println("Checking Box");
-        driver.findElement(By.name("li1")).click();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            return response.toString();
+        }
+    }
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li2")).click();
+    private String getPOSTApiResponse(String apiUrl, String requestBody) throws IOException {
+        URL url = new URL(apiUrl);
+        String apiKey = "cd9a7315357f0e8f6866ba50515928617182c4d8f58fec0b1397c8534c93537f73bf8fad9d0be32f4f6d07d26502f6949780e00365547ccfc7dfc56b33d3382d616a1443aa0cc38531566209d5c33d75";
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("apiKey", apiKey);
+        connection.setDoOutput(true);
 
-        System.out.println("Checking Box");
-        driver.findElement(By.name("li3")).click();
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = requestBody.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li4")).click();
+        int responseCode = connection.getResponseCode();
+        if (responseCode >= HttpURLConnection.HTTP_BAD_REQUEST) {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getErrorStream()))) {
+                StringBuilder errorResponse = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    errorResponse.append(inputLine);
+                }
+                throw new IOException("Error: " + errorResponse.toString());
+            }
+        }
 
-        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 6");
-        driver.findElement(By.id("addbutton")).click();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            return response.toString();
+        }
+    }
 
-        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 7");
-        driver.findElement(By.id("addbutton")).click();
+    private void getPUTApiResponse(String apiUrl, BigInteger dynamicId) throws IOException {
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 8");
-        driver.findElement(By.id("addbutton")).click();
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("executionResultId", dynamicId);
+        String requestBody = jsonBody.toString();
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li1")).click();
+        String apiKey = "cd9a7315357f0e8f6866ba50515928617182c4d8f58fec0b1397c8534c93537f73bf8fad9d0be32f4f6d07d26502f6949780e00365547ccfc7dfc56b33d3382d616a1443aa0cc38531566209d5c33d75";
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li3")).click();
+        connection.setRequestMethod("PUT");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("apiKey", apiKey);
+        connection.setDoOutput(true);
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li7")).click();
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = requestBody.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li8")).click();
-        Thread.sleep(300);
+        int responseCode = connection.getResponseCode();
+    }
 
-        System.out.println("Entering Text");
-        driver.findElement(By.id("sampletodotext")).sendKeys("Get Taste of Lambda and Stick to It");
+    private String getAddExecutionResultResponse(String apiUrl, String userTestResultStatus) throws IOException {
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        driver.findElement(By.id("addbutton")).click();
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("color", userTestResultStatus.equals("passed") ? "#BAB86C" : "#800000");
+        jsonBody.put("name", userTestResultStatus);
+        jsonBody.put("description", "Test result updated according to the USER's test case result");
+        String requestBody = jsonBody.toString();
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li9")).click();
+        String apiKey = "cd9a7315357f0e8f6866ba50515928617182c4d8f58fec0b1397c8534c93537f73bf8fad9d0be32f4f6d07d26502f6949780e00365547ccfc7dfc56b33d3382d616a1443aa0cc38531566209d5c33d75";
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("apiKey", apiKey);
+        connection.setDoOutput(true);
 
-        // Let's also assert that the todo we added is present in the list.
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = requestBody.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
 
-        spanText = driver.findElementByXPath("/html/body/div/div/div/ul/li[9]/span").getText();
-        Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);
-        Status = "passed";
-        Thread.sleep(150);
+        int responseCode = connection.getResponseCode();
 
-        System.out.println("TestFinished");
+        BufferedReader in;
+        if (responseCode >= HttpURLConnection.HTTP_BAD_REQUEST) {
+            in = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        } else {
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        }
 
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
+    }
+
+    private Map<String, String> getStatusIndFromJsonResponse(String jsonResponse) {
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+        JSONObject dataObject = jsonObject.getJSONObject("data");
+        String testName =  dataObject.optString("name", "unknown");
+        String status =  dataObject.optString("status_ind", "unknown");
+        // Create a map to hold the results
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("testName", testName);
+        resultMap.put("status", status);
+        
+        return resultMap; // Return the map
+    }
+
+    private BigInteger getExecutionResultUsersStatus(String jsonResponse) {
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+        return jsonObject.optBigInteger("id", BigInteger.valueOf(0));
+    }
+
+    private JSONArray getSearchTestCycleResponse(String jsonResponse) {
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+        return jsonObject.getJSONArray("data");
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.executeScript("lambda-status=" + Status);
-        driver.quit();
-    }
+        try {
+            if (driver != null) {
+                driver.executeScript("lambda-status=" + testStatus);
+                SessionId session = driver.getSessionId();
+                String sessionApiUrl = "https://api.lambdatest.com/automation/api/v1/sessions/" + session;
+                String authKey = "Basic ZGVlcGFuc2h1bGFtYmRhdGVzdDpTTUt3cGRudFJoQnBVYmRMclVxazB0VTBUakFacXVWOWt6MFl4bk9XVW81NkVhcXRRdw==";
+                String response = getGETApiResponse(sessionApiUrl, "Authorization", authKey);
+                Map<String, String> result = getStatusIndFromJsonResponse(response);
+                String userTestResultStatus = result.get("status");
+                String userTestResultName = result.get("testName");
 
+                // POST API for getting test cycle
+                String searchTestCycleApiUrl = "https://qtmcloud.qmetry.com/rest/api/latest/testcycles/search/";
+                String searchTestCycleResponse = getPOSTApiResponse(searchTestCycleApiUrl, "{\"filter\":{\"projectId\":10000}}");
+                JSONArray searchTestCycleResult = getSearchTestCycleResponse(searchTestCycleResponse);
+                String testCycleId = "";
+                for (int i = 0; i < searchTestCycleResult.length(); i++) {
+                    JSONObject obj = searchTestCycleResult.getJSONObject(i);
+                    String key = obj.getString("key");
+                    if ("SCRUM-TR-9".equals(key)) {
+                        String id = obj.getString("id");
+                        testCycleId = id;
+                        break;
+                    }
+                }
+
+                // POST API for get linked test cases of test cycle
+                String getLinkedTestCasesApiUrl = "https://qtmcloud.qmetry.com/rest/api/latest/testcycles/" + testCycleId + "/testcases/search/";
+                String linkedTestCasesResponse = getPOSTApiResponse(getLinkedTestCasesApiUrl, "{\n" +
+                        "    \"filter\": {\n" +
+                        "        \"status\": [\n" +
+                        "            \"In Progress\",\n" +
+                        "            \"To Do\",\n" +
+                        "            \"Passed\",\n" +
+                        "            \"Failed\",\n" +
+                        "            \"Blocked\",\n" +
+                        "            \"Not Executed\"\n" +
+                        "        ]\n" +
+                        "    }\n" +
+                        "}");
+
+                JSONArray linkedTestCasesResult = getSearchTestCycleResponse(linkedTestCasesResponse);
+                BigInteger testCaseExecutionId = BigInteger.valueOf(0);
+                for (int i = 0; i < linkedTestCasesResult.length(); i++) {
+                    JSONObject obj = linkedTestCasesResult.getJSONObject(i);
+                    String key = obj.getString("key");
+                    if (userTestResultName.equals(key)) {
+                        BigInteger id = BigInteger.valueOf(obj.getLong("testCaseExecutionId"));
+                        System.out.println("----------LT  id-----------" + id);
+                        testCaseExecutionId = id;
+                        break;
+                    }
+                }
+                // JSONObject linkedTestCaseResponseObj = linkedTestCasesResult.getJSONObject(0);
+                // BigInteger testCaseExecutionId = linkedTestCaseResponseObj.getBigInteger("testCaseExecutionId");
+
+                // GET execution result
+                String getExecutionResultApiUrl = "https://qtmcloud.qmetry.com/rest/api/latest/projects/10000/execution-results";
+                String apiKey = "cd9a7315357f0e8f6866ba50515928617182c4d8f58fec0b1397c8534c93537f73bf8fad9d0be32f4f6d07d26502f6949780e00365547ccfc7dfc56b33d3382d616a1443aa0cc38531566209d5c33d75";
+                String getExecutionResultResponse = getGETApiResponse(getExecutionResultApiUrl, "apiKey", apiKey);
+                JSONArray jsonArray = new JSONArray(getExecutionResultResponse);
+                BigInteger executionResultId = BigInteger.valueOf(0);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject executionResultResponseObj = jsonArray.getJSONObject(i); // get the individual obj
+                    String name = executionResultResponseObj.getString("name");
+                    if (userTestResultStatus.equals(name)) {
+                        executionResultId = executionResultResponseObj.getBigInteger("id");
+                        break;
+                    }
+                }
+                if (executionResultId.equals(BigInteger.valueOf(0))) {
+                    String addExecutionResultApiUrl = "https://qtmcloud.qmetry.com/rest/api/latest/projects/10000/execution-results";
+                    String addExecutionResultResponse = getAddExecutionResultResponse(addExecutionResultApiUrl, userTestResultStatus);
+                    executionResultId = getExecutionResultUsersStatus(addExecutionResultResponse);
+                }
+
+                String updateTestCaseExecutionApiUrl = "https://qtmcloud.qmetry.com/rest/api/latest/testcycles/" + testCycleId + "/testcase-executions/" + testCaseExecutionId;
+                getPUTApiResponse(updateTestCaseExecutionApiUrl, executionResultId);
+            }
+        } catch (Exception e) {
+            System.out.println("----------EXCEPTIONS-----------" + e);
+        } finally {
+            if (driver != null) {
+                driver.quit(); // Ensure driver quits
+            }
+        }
+    }
 }
